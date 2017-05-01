f9c2e3c88ebabebe84c2d448165fc1e2e939db43
Lee Lovejoy <ll2833@columbia.edu>
2017-05-01 13:39:21 -0400

    updates to trial manager

--- @trialManager/trialManager.m
+++ @trialManager/trialManager.m
@@ -15,15 +15,25 @@ classdef trialManager < handle
     %  element is a list of specifiers for that trial.  The list is
     %  searchable to obtain an element of trialIndex
     
-    properties %(Access = protected)
+    properties
+        conditions
         trialNumber = 0;
-        repetitionNumber = 0;
-        trialIndex
         trialList
         maxTrials
-        maxRepetitions
         trialTokens
-        repeatProbability = 0;
+        sequentialErrors
+        sequentialCorrects
+        inCorrectionLoop = false;
+        correctionLoopPool
+        correctionLoopTokens
+        correctionLoopTrialNumber = 0;
+        repeat = false;
+        maxSequentialErrors
+        minSequentialCorrects = 1;
+    end
+    
+    properties (Dependent)
+        trialIndex
     end
     
     methods
@@ -33,32 +43,126 @@ classdef trialManager < handle
             for i=1:2:nargin
                 obj.(varargin{i}) = varargin{i+1};
             end
-            obj.trialIndex = 1;            
-            obj.trialList = 1:obj.maxTrials;
-        end     
-                
-        %  indexTrialSpecifiers
+            if(~any(strcmp('maxTrials',varargin(1:2:nargin))))
+                obj.maxTrials = numel(obj.conditions);
+            end
+            obj.trialList = randperm(obj.maxTrials);
+            obj.sequentialErrors = zeros(obj.maxTrials,1);
+        end
+        
+        %  nextTrial
         %
-        %  given a cell array of conditions, create a cell array of trial
-        %  specifiers which we can search to obtain a trial index
-        function obj = indexTrialSpecifiers(obj,conditions,specifiers)
-                obj.trialTokens = cell(length(conditions),1);
-                for i=1:length(obj.trialTokens)
-                    obj.trialTokens{i} = cell(length(specifiers),1);
-                    for j=1:length(specifiers)
-                        obj.trialTokens{i}{j} = conditions{i}.(specifiers{j});
+        %  Give us the next trial please
+        function condition = nextTrial(obj)
+            if(~obj.inCorrectionLoop)
+                if(~obj.repeat)
+                    obj.trialNumber = obj.trialNumber + 1;
+                else
+                    obj.repeat = false;
+                end
+                condition = obj.conditions{obj.trialIndex};
+            else
+                obj.correctionLoopTrialNumber = obj.correctionLoopTrialNumber + 1;
+                condition = obj.conditions{obj.correctionLoopPool(unidrnd(length(obj.correctionLoopPool)))};
+            end
+        end
+        
+        %  Get method for trialIndex
+        function value = get.trialIndex(obj)
+            value = obj.trialList(obj.trialNumber);
+        end
+        
+        %  tokenize
+        %
+        %  given a list of specifiers, create a cell array of trial
+        %  tokens based on the specifiers
+        function obj = tokenize(obj,varargin)
+            specifiers = varargin;
+            obj.trialTokens = cell(numel(obj.conditions),1);
+            for i=1:numel(obj.conditions)
+                for j=1:numel(specifiers)
+                    if(~isempty(obj.conditions{i}.(specifiers{j})))
+                        if(~iscell(obj.conditions{i}.(specifiers{j})))
+                            obj.trialTokens{i}{j} = obj.conditions{i}.(specifiers{j});
+                        elseif(iscell(obj.conditions{i}.(specifiers{j})))
+                            obj.trialTokens{i}{j} = obj.conditions{i}.(specifiers{j}){:};
+                        end
                     end
                 end
             end
+        end        
         
-        %  update
+        %  repeatTrial
         %
-        %  Update the trial and block count
-        function update(obj,varargin)
-            obj.trialNumber = obj.trialNumber + 1;
-            obj.trialIndex = obj.trialList(obj.trialNumber+1);
-            obj.repetitionNumber = 0;
+        %  set repeat flag
+        function repeatTrial(obj)
+            obj.repeat = true;
         end
+        
+        %  checkCorrectionLoopEntry
+        %
+        %  check for correction loop entry conditions
+        function checkCorrectionLoopEntry(obj,correct)
+            
+            if(correct)
+                
+                %  Reset the sequential error counter
+                obj.sequentialErrors(obj.trialIndex) = 0;                
+            else
+                
+                %  Increment the sequential error counter
+                obj.sequentialErrors(obj.trialIndex) = obj.sequentialErrors(obj.trialIndex)+1;
+                
+                %  Match level between current trial's tokens and those in the
+                %  tokens list (including the current trial)
+                maxLevel = length(obj.trialTokens{obj.trialIndex});
+                matchLevels = maxLevel*ones(obj.maxTrials,1);
+                for i=1:obj.maxTrials
+                    while (matchLevels(i)>0 && ~isempty(setdiff(obj.trialTokens{obj.trialIndex}(1:matchLevels(i)),obj.trialTokens{i})));
+                        matchLevels(i) = matchLevels(i)-1;
+                    end
+                end
+                
+                %  Check sequential error totals against the max by descending match level
+                level = max(matchLevels);
+                while(level>0 && ~obj.inCorrectionLoop)
+                    ix = matchLevels==level;
+                    obj.inCorrectionLoop = sum(obj.sequentialErrors(ix)) >= obj.maxSequentialErrors;
+                    level = level-1;
+                end
+                if(obj.inCorrectionLoop)
+                    obj.correctionLoopTokens = obj.trialTokens{obj.trialIndex}(1:level+1);
+                    obj.correctionLoopPool = find(ix);
+                    obj.sequentialErrors(ix) = 0;
+                    obj.sequentialCorrects = 0;
+                end
+            end
+        end
+        
+        %  checkCorrectionLoopExit
+        %
+        %  check for correction loop exit conditions
+        function checkCorrectionLoopExit(obj,correct)
+            
+            if(correct)
+                obj.sequentialCorrects = obj.sequentialCorrects + 1;
+                
+                if(obj.sequentialCorrects >= obj.minSequentialCorrects)
+                    obj.inCorrectionLoop = false;
+                    obj.correctionLoopTrialNumber = 0;
+                end
+            else
+                obj.sequentialCorrects = 0;
+            end
+        end
+        
+        %  exitCorrectionLoop
+        %
+        %  Reset correction loop flag
+        function exitCorrectionLoop(obj)
+            obj.inCorrectionLoop = false;
+            obj.correctionLoopTrialNumber = 0;
+        end        
         
         %  checkRunTerminationCriteria
         %
@@ -79,7 +183,7 @@ classdef trialManager < handle
         %  shuffleRemainingTrials
         %
         %  shuffle remaining trials; if additional inputs
-        %  provided then shuffle trials specified
+        %  provided then
         function shuffleRemainingTrials(obj,varargin)
             
             %  Determine which trials to shuffle
@@ -89,18 +193,7 @@ classdef trialManager < handle
             obj.trialList(ix) = Shuffle(obj.trialList(ix));
             
             %  We are starting new
-            obj.repetitionNumber = 0;
             obj.trialIndex = obj.trialList(obj.trialNumber + 1);
-        end 
-
-        %  repeatTrial
-        %
-        %  Return boolean indicating whether or not to repeat triail
-        function outcome = repeatTrial(obj)
-            outcome = logical(binornd(1,obj.repeatProbability)) && obj.repetitionNumber < obj.maxRepetitions;
-            if(outcome)
-                obj.repetitionNumber = obj.repetitionNumber + 1;
-            end
         end
         
     end
